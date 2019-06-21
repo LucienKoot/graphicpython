099bc16f99954287cd7106558e440c7148ff52d6
Ludelaplu <lucien.koot@gmail.com>
2019-06-21 16:04:31 +0200

    draw7.

---
+++ draw7.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.color('orange')
+turtle.bgcolor('darkgreen')
+turtle.begin_fill()
+for i in range (9):
+    turtle.forward(200)
+    turtle.left(100)
+    turtle.end_fill()
+    
