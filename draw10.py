3d3667fdafae4cf60614e3d50ece83bc5c5dad2d
Ludelaplu <lucien.koot@gmail.com>
2019-06-22 11:48:15 +0200

    gr10

---
+++ draw10.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.color('gold')
+turtle.pensize(2)
+turtle.bgcolor('OrangeRed')
+
+turtle.begin_fill()
+
+for i in range (9):
+        
+        turtle.forward (200)
+        turtle.left(200)
+turtle.end_fill()
+
+    
