fbaa954b61cffcbc76afc9962f178eb720ac079f
Ludelaplu <lucien.koot@gmail.com>
2019-06-23 19:14:52 +0200

    moving target

---
+++ draw11.py
@@ -0,0 +1,29 @@
+import turtle
+turtle.color("blue")
+turtle.begin_fill()
+for i in range (5):
+    turtle.forward(100)
+    turtle.left(144)
+turtle.end_fill()
+
+turtle.up()
+turtle.setposition(0, 100)
+turtle.down()
+
+turtle.color("red")
+turtle.begin_fill()
+for i in range (5):
+    turtle.forward(100)
+    turtle.left(144)
+turtle.end_fill()
+
+turtle.up()
+turtle.setposition(-150, 150)
+turtle.down()
+
+turtle.color("green")
+turtle.begin_fill()
+for i in range (5):
+    turtle.forward(100)
+    turtle.left(144)
+turtle.end_fill()
