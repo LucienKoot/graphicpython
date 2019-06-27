10910bf45090e26d7b04ec7a8d4f6e52e2b03fd3
Ludelaplu <lucien.koot@gmail.com>
2019-06-27 11:07:32 +0200

    drawvar whileloop2

--- drawreekswhileloop1.py
+++ drawreekswhileloop1.py
@@ -2,8 +2,11 @@
 	
 import turtle
 
+turtle.pensize (7)
+turtle.color ("gold")
+
 h = 0
 while h  < 4:
-    turtle.forward(100)
+    turtle.forward(150)
     turtle.left(90)
-    h = h + 1
+    h += 1
