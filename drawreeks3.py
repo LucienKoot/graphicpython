3cf97fe254ae133a6c7d8ba7e064916dd403278d
Ludelaplu <lucien.koot@gmail.com>
2019-06-27 10:30:21 +0200

    drawvar1

--- drawreeks3.py
+++ drawreeks3.py
@@ -1,8 +1,14 @@
 import turtle
 stamp = turtle.Turtle()
-stamp.shape("circle")
+stamp.shape("triangle")
 
 stamp.penup()
+stamp.forward(50)
+stamp.stamp()
+
+stamp.forward(50)
+stamp.stamp()
+
 stamp.forward(50)
 stamp.stamp()
 
