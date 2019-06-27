1fe02d388578185abaabe6f9957ed35e645c897b
Ludelaplu <lucien.koot@gmail.com>
2019-06-27 10:28:50 +0200

    drawvar

--- drawreeks2.py
+++ drawreeks2.py
@@ -1,15 +1,15 @@
 import turtle
-elsa=turtle.Turtle()
+e=turtle.Turtle()
 wn=turtle.Screen()
 wn.bgcolor("black")
-elsa.color ("orange")
-elsa.pensize (6)
+e.color ("orange")
+e.pensize (6)
 for x in range (12):
     for x in range (2):
-        elsa.forward(100)
-        elsa.right(60)
-        elsa.forward(100)
-        elsa.right(120)
-    elsa.right (30)
+        e.forward(100)
+        e.right(60)
+        e.forward(100)
+        e.right(120)
+    e.right (30)
     
     
---
+++ drawreeks3.py
@@ -0,0 +1,13 @@
+import turtle
+stamp = turtle.Turtle()
+stamp.shape("circle")
+
+stamp.penup()
+stamp.forward(50)
+stamp.stamp()
+
+stamp.forward(50)
+stamp.stamp()
+
+stamp.forward(50)
+stamp.stamp()
