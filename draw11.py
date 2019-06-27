78f7568835e41b3a29c989774d11bc15b8f95218
Ludelaplu <lucien.koot@gmail.com>
2019-06-27 07:58:48 +0200

    drawforloop

--- draw11.py
+++ draw11.py
@@ -7,7 +7,7 @@ for i in range (5):
 turtle.end_fill()
 
 turtle.up()
-turtle.setposition(0, 100)
+turtle.setposition(0, 120)
 turtle.down()
 
 turtle.color("red")
@@ -18,7 +18,7 @@ for i in range (5):
 turtle.end_fill()
 
 turtle.up()
-turtle.setposition(-150, 150)
+turtle.setposition(-130, 160)
 turtle.down()
 
 turtle.color("green")
@@ -34,6 +34,17 @@ turtle.setposition(-200, 50)
 turtle.down()
 
 turtle.color("orange")
+turtle.begin_fill()
+for i in range (5):
+    turtle.forward (100)
+    turtle.left(144)
+turtle.end_fill()
+
+turtle.up()
+turtle.setposition(-110, -35)
+turtle.down()
+
+turtle.color("purple")
 turtle.begin_fill()
 for i in range (5):
     turtle.forward (100)
